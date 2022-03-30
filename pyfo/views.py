f34066a5bc4c59cb5ce594260e387c4c1d651a92
Fola Flor <dpcalfola@gmail.com>
2022-03-30 12:09:52 +0900

    Study > static doc in Django
    Should to add this code bottom of "STATIC_URL = 'static/'"
    
    	STATICFILES_DIRS = [
        		BASE_DIR / 'static'
    	]

--- pyfo/views.py
+++ pyfo/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from rest_framework.views import APIView
-from .models import Question
+from .models import Question, Answer
 
 
 # Create your views here.
@@ -32,6 +32,7 @@ def detail(request, question_id):
     return render(request, 'pyfo/question_detail.html', context)
 
 
+# 사용하지 않음
 def answer_create(request, question_id):
     """
     pyfo 답변 등록
@@ -39,4 +40,18 @@ def answer_create(request, question_id):
 
     question = get_object_or_404(Question, pk=question_id)
     question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pyfo:detail', question_id=question.id)
+
+
+# uils.py 에서 answer_create_02 를 사용함
+def answer_create_02(request, question_id):
+    """
+    pyfo 답변등록, Answer 모델 이용
+    :param request: Question object
+    :param question_id: question.id
+    :return: redirect(pyfo:detail, question_id=question.id)
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    answer.save()
     return redirect('pyfo:detail', question_id=question.id)
