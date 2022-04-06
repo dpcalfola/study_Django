6867664d799ba0007fb72bc6637cfd6827f01e6e
Fola Flor <dpcalfola@gmail.com>
2022-04-07 00:26:26 +0900

    Feat > Bootstrap-v5 - apply bootstrap_form

--- Django_web_05/config/settings.py
+++ Django_web_05/config/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap5',
+
     'accountapp.apps.AccountappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
