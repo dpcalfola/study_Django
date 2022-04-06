953318b0a09d474843305872de04aa3ad2287a1d
Fola Flor <dpcalfola@gmail.com>
2022-04-06 19:12:08 +0900

    Feat > Startapp accountapp

--- Django_web_05/config/settings.py
+++ Django_web_05/config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accountapp.apps.AccountappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
