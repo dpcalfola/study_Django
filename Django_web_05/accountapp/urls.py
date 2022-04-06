3c6b85cd82ac618b0eba942bc61fc0eda25c8289
Fola Flor <dpcalfola@gmail.com>
2022-04-06 21:46:38 +0900

    Feat > Signup, Login

--- Django_web_05/accountapp/urls.py
+++ Django_web_05/accountapp/urls.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
 
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('', views.hello_world, name="hello_world"),
+    path('', views.hello_world, name='hello_world'),
+    path('create/', views.AccountCreateView.as_view(), name='create'),
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
